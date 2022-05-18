1e9f5d86f520cf3cf00719e6c9468b1e74cedd5a
andresdelrio <andresdelrio@192.168.0.2>
2022-05-18 11:21:27 -0400

    Habían registros duplicados debido a espacios en nombres y apellidos

--- Transacciones-Personas-Sujetas/tp01.sql
+++ Transacciones-Personas-Sujetas/tp01.sql
@@ -116,13 +116,13 @@ SELECT
 LEFT JOIN
     (
         SELECT
-            CASE 
+            DISTINCT CASE 
                 WHEN CONINT.NPASAPORTE IS NULL THEN TRIM(CONINT.RUT) 
                 ELSE TRIM(CONINT.NPASAPORTE) 
             END IDENTIFICADOR,
-            CONINT.NOMBRES NOMBRE,
-            CONINT.APELLIDOP APELLIDO_PATERNO,
-            CONINT.APELLIDOM APELLIDO_MATERNO 
+            trim(regexp_replace(CONINT.NOMBRES, '\s+', '', 'g')) NOMBRE,
+            trim(regexp_replace(CONINT.APELLIDOP, '\s+', '', 'g')) APELLIDO_PATERNO,
+            trim(regexp_replace(CONINT.APELLIDOM, '\s+', '', 'g')) APELLIDO_MATERNO 
         FROM
             WFPROCESS WFP  
         LEFT OUTER JOIN
